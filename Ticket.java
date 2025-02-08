5393676f9b13a78bda79ff59c1f48da3a73d6ccb
Momo <pr0h3ckr@gmail.com>
2025-02-08 16:48:56 +0000

    ticket foundation modelled

---
+++ Ticket.java
@@ -0,0 +1,15 @@
+public class Ticket {
+    String firstName;
+    String lastName;
+    String email;
+    String seatPos;
+    String ticketID;
+
+    Ticket(String firstName,String lastName, String email, String seatPos, String ticketID){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.seatPos = seatPos;
+        this.ticketID = ticketID;
+    }
+}
--- main.java
+++ main.java
@@ -1,5 +1,11 @@
-class main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+import UI.*;
+
+public class Main {
+    public static void main(String[] args){
+        Ticket ticket = new Ticket("John", "Smith", "BLHA@gsoadns.com", "3z", "98yas9hdub");
+        System.out.println(ticket.firstName+" " +ticket.lastName);
+        System.out.println(ticket.email);
+        System.out.println(ticket.seatPos);
+        System.out.println(ticket.ticketID);
+    }
 }
